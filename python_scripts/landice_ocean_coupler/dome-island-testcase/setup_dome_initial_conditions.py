036f51e09f6f8ad3a5298206102984339156d041
Matthew Hoffman <mhoffman@lanl.gov>
2013-05-14 11:57:14 -0600

    Changing dome dimensions
    Making it wider and flatter for more ice-ocean contact!

--- python_scripts/landice_ocean_coupler/dome-island-testcase/setup_dome_initial_conditions.py
+++ python_scripts/landice_ocean_coupler/dome-island-testcase/setup_dome_initial_conditions.py
@@ -58,8 +58,8 @@ except:
 
 # Assign variable values for dome
 # Define dome dimensions - all in meters
-r0 = 200000.0 * sqrt(0.125)
-h0 = 5000.0 * sqrt(0.125)
+r0 = 325000.0 * sqrt(0.125)
+h0 = 3250.0 * sqrt(0.125)
 #x0 = 30000.0
 #y0 = 30000.0
 x0 = xCell.min() + 0.5 * (xCell.max() - xCell.min() )
